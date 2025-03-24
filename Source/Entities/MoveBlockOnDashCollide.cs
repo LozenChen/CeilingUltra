c56065ff1e60defc02bde9616335981ce869f789
Lozen <865466388@qq.com>
2025-03-24 20:15:09 +0800

    add MoveBlockOnDashCollide

---
+++ Source/Entities/MoveBlockOnDashCollide.cs
@@ -0,0 +1,23 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.CeilingUltra.Entities;
+
+[CustomEntity("CeilingUltra/MoveBlockOnDashCollide")]
+public class MoveBlockOnDashCollide : MoveBlock {
+
+    // we don't need this field, but leave it here so others can find it easily
+    public bool ActivateOnDashCollide = true;
+    public MoveBlockOnDashCollide(EntityData data, Vector2 offset)
+        : base(data, offset) {
+        ActivateOnDashCollide = data.Bool("ActivateOnDashCollide", true);
+        if (ActivateOnDashCollide) {
+            OnDashCollide = ActivateOnDash;
+        }
+    }
+
+    public DashCollisionResults ActivateOnDash(Player player, Vector2 direction) {
+        triggered = true;
+        return DashCollisionResults.NormalCollision;
+    }
+}
